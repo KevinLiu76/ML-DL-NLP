e3be0d875dcce922da7173fb44b18a130782969d
KevinLiu76 <lwk112338413@gmail.com>
2023-09-24 14:34:25 +0800

    knn example

--- ch01_KNN/2.1-knn.py
+++ ch01_KNN/2.1-knn.py
@@ -1,6 +1,21 @@
 from numpy import *
 import operator
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines() # 读取所有行
+    numberOfLines = len(arrayOLines) # 得到文件行数
+    returnMat = zeros((numberOfLines,3)) # 创建返回的numpy矩阵
+    classLabelVector = [] # 创建返回的分类标签向量
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t') # 将每行数据分割成一个列表
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]]) # 4*2
     labels = ['A','A','B','B']
